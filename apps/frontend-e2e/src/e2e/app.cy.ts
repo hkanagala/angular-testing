79bd2b8a66ff588ec6b9bb258bc293d102ca4277
Jeffry Gonzalez <jeff@hypertheory.com>
2022-10-04 12:30:06 -0400

    started with home components

--- apps/frontend-e2e/src/e2e/app.cy.ts
+++ apps/frontend-e2e/src/e2e/app.cy.ts
@@ -1,6 +1,14 @@
 describe('frontend', () => {
   beforeEach(() => cy.visit('/'));
 
+  describe('Routing Defaults', () => {
+    it('Updates the location', () => {
+      cy.url().should('match', /\/home$/); // at the end of the url ($), there should be a '/' (\/) followed by 'home'
+    });
+    it('Displays the Home Component', () => {
+      cy.get('[data-testid="app-home"]').should('exist');
+    });
+  });
   it('The Header Is Set', () => {
     // Custom command example, see `../support/commands.ts` file
     cy.get('[data-testid="app-mastheader-title"]').contains(
--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -4,10 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { MastheadComponent } from './components/masthead/masthead.component';
+import { HomeComponent } from './components/home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
+];
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, MastheadComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    MastheadComponent,
+    HomeComponent,
+  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
 })
